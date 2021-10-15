72f64b2d1d73a280f9ddac0e08e63cd9ecc4b0a9
safonov_dmitriy <sdssds341@gmail.com>
2021-10-15 17:26:45 +0300

    [add] scvSorting test

---
+++ src/util/helpers/scvSorting.test.ts
@@ -0,0 +1,64 @@
+import scvSorting from "./scvSorting";
+
+describe("scvSorting", () => {
+  it("sort data for table", async () => {
+    const withoutSortData = [
+      ["Names", "Pkg1", "Pkg2", "Pkg3"],
+      ["Price (€)", "5", "50", "100"],
+      [
+        "Description",
+        "Pkg1 description",
+        "Pkg2 description",
+        "Pkg3 description",
+      ],
+      ["", "", "", ""],
+      ["Reporting", "", "", ""],
+      ["Detailed Reports", "-", "X", "X"],
+      ["Weekly Data Reports", "-", "X", "X"],
+      ["Create your own reports", "X", "X", "X"],
+      ["", "", "", ""],
+      ["Blog", "", "", ""],
+      ["Create your own blog", "X", "X", "X"],
+      ["Upload Images", "-", "X", "X"],
+      ["", "", "", ""],
+      ["File Storage", "", "", ""],
+      ["Upload files", "-", "X", "-"],
+      ["Share files with team", "X", "X", "X"],
+      ["Watch Videos", "-", "-", "-"],
+      ["", "", "", ""],
+      ["Application Intergrations", "", "", ""],
+      ["Install new apps", "-", "X", "X"],
+      ["Install custom apps", "X", "-", "-"],
+    ];
+    const sortData = [
+      ["Names", "Pkg1", "Pkg2", "Pkg3"],
+      ["Price (€)", "5", "50", "100"],
+      [
+        "Description",
+        "Pkg1 description",
+        "Pkg2 description",
+        "Pkg3 description",
+      ],
+      ["", "", "", ""],
+      ["Application Intergrations", "", "", ""],
+      ["Install custom apps", "X", "", ""],
+      ["Install new apps", "", "X", "X"],
+      ["", "", "", ""],
+      ["Blog", "", "", ""],
+      ["Create your own blog", "X", "X", "X"],
+      ["Upload Images", "", "X", "X"],
+      ["", "", "", ""],
+      ["File Storage", "", "", ""],
+      ["Share files with team", "X", "X", "X"],
+      ["Upload files", "", "X", ""],
+      ["Watch Videos", "", "", ""],
+      ["", "", "", ""],
+      ["Reporting", "", "", ""],
+      ["Create your own reports", "X", "X", "X"],
+      ["Detailed Reports", "", "X", "X"],
+      ["Weekly Data Reports", "", "X", "X"],
+    ];
+
+    expect(scvSorting(withoutSortData)).toStrictEqual(sortData);
+  });
+});
--- src/util/helpers/svcParser.test.tsx
+++ src/util/helpers/svcParser.test.tsx
@@ -6,7 +6,6 @@ global.TextDecoder = TextDecoder;
 describe("scvSorting", () => {
   it("return data for render table", async () => {
     const data = await svcParser("/data/data.csv");
-    console.log(`data`, data);
     expect(data.length).not.toBe(0);
   });
 });
