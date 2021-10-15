29fb5cde2eb19b76be800f6bc4f11cd364a38e9a
safonov_dmitriy <sdssds341@gmail.com>
2021-10-15 17:17:37 +0300

    [add] tests

--- src/components/Table/Table.test.tsx
+++ src/components/Table/Table.test.tsx
@@ -1,9 +1,11 @@
+import { render } from "@testing-library/react";
 import React from "react";
 
 import { Table } from "..";
 describe("Table", () => {
   it("render Table without crash", () => {
-    // const data = [["test", "test", "test", "test"]];
-    // const component = renderer.create(<Table data={data} />);
+    const data = [["test", "test", "test", "test"]];
+    const { container } = render(<Table data={data} />);
+    expect(container).toBeInTheDocument();
   });
 });
--- src/util/helpers/svcParser.test.tsx
+++ src/util/helpers/svcParser.test.tsx
@@ -1,14 +1,12 @@
-import { render } from "@testing-library/react";
-import React from "react";
-import { Provider } from "react-redux";
+import { TextDecoder } from "util";
 import svcParser from "./svcParser";
-import { TextEncoder, TextDecoder } from "util";
 
 //@ts-ignore
 global.TextDecoder = TextDecoder;
 describe("scvSorting", () => {
-  //   it("return data for render table", async () => {
-  //     const data = await svcParser("/data/data.csv");
-  //     console.log(`data`, data);
-  //   });
+  it("return data for render table", async () => {
+    const data = await svcParser("/data/data.csv");
+    console.log(`data`, data);
+    expect(data.length).not.toBe(0);
+  });
 });
