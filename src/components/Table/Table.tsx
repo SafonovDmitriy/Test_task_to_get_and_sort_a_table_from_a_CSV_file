0fb8211c490064afd269a98b176e20e9921296ab
safonov_dmitriy <sdssds341@gmail.com>
2021-10-20 09:34:08 +0300

    [fix] Table styles

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -14,16 +14,29 @@ const changeSymbol = (symbol: string) => {
 };
 const Table = () => {
   const data: string[][] = useSelector(dataForTableSelector);
+  const arrayLengthWithoutEmptyElements = (tr: string[]) => {
+    return tr.filter((item) => item).length;
+  };
   return (
     <table className="app_table">
       <tbody>
-        {data.map((tr, idxTr) => (
-          <tr key={idxTr}>
-            {tr.map((td, idxTd) => (
-              <td key={`${idxTr}-${idxTd}`}>{changeSymbol(td)}</td>
-            ))}
-          </tr>
-        ))}
+        {data.map((tr, idxTr) => {
+          console.log(`tr`, tr);
+          console.log(arrayLengthWithoutEmptyElements(tr));
+
+          return (
+            <tr
+              key={idxTr}
+              className={
+                arrayLengthWithoutEmptyElements(tr) === 1 ? "thumbnail" : ""
+              }
+            >
+              {tr.map((td, idxTd) => (
+                <td key={`${idxTr}-${idxTd}`}>{changeSymbol(td)}</td>
+              ))}
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
