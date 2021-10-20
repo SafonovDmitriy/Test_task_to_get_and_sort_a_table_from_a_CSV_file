9507fd719882cead5fc633ba1a265a4082e1dcad
safonov_dmitriy <sdssds341@gmail.com>
2021-10-20 16:50:02 +0300

    [refactor] Pricing Table

--- src/components/PricingTables/PricingTables.tsx
+++ src/components/PricingTables/PricingTables.tsx
@@ -1,4 +1,4 @@
-import { FC, memo } from "react";
+import { FC } from "react";
 import { SYMBOLS_MAP } from "../../constants";
 import "./PricingTablesStyle.css";
 
@@ -9,20 +9,18 @@ interface IPricingTablesProps {
   dataForPricingTables: Array<string[]>;
 }
 
-const PricingTables: FC<IPricingTablesProps> = memo(
-  ({ dataForPricingTables }) => (
-    <table className="app_table">
-      <tbody>
-        {dataForPricingTables.map((tr, idxTr) => (
-          <tr key={idxTr} className={checkIfGroup(tr) ? "boldText" : ""}>
-            {tr.map((td, idxTd) => (
-              <td key={`${idxTr}-${idxTd}`}>{SYMBOLS_MAP[td] || td}</td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  )
+const PricingTables: FC<IPricingTablesProps> = ({ dataForPricingTables }) => (
+  <table className="app_table">
+    <tbody>
+      {dataForPricingTables.map((tr, idxTr) => (
+        <tr key={idxTr} className={checkIfGroup(tr) ? "boldText" : ""}>
+          {tr.map((td, idxTd) => (
+            <td key={`${idxTr}-${idxTd}`}>{SYMBOLS_MAP[td] || td}</td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  </table>
 );
 
 export default PricingTables;
